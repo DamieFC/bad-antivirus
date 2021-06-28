22c993c09ae32ed755e4f19f20604a790da2f48d
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-28 10:13:27 -0700

    The useless antivirus, oh, I'm sorry, COMPLETELY SAFE AND GOOD ANTIVIRUS itself
    Jello Biafra attitude

---
+++ src/antivirus.c
@@ -0,0 +1,12 @@
+/********************************************************************
+ * This code is inspired from a discussion on Discord, which is     *
+ * licensed under the GPLv3.0                                       *
+ * (c) 2021 DamieFC                                                 *
+ ********************************************************************/
+#include <stdio.h>
+
+int main(int argc, char argv[]){
+  printf("Your computer is safe!\n");
+  printf("This code is licensed under the GPLv3.0 license.");
+  return(0);
+}
